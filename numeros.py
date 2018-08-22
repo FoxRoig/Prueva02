d96d71a389c94e7eee62ff5f8910717386e536a1
FoxRoig <lmnfoxhound@gmail.com>
2018-08-22 22:10:00 +0200

    Los numeros enteros son denominados "int", los numeros con decimales "float", los textos "str" o "string", si por ejemplo tenemos un "float" y queremos hacer que sea un "int" eliminandole los decimales pondremos "int()", dentro de los parentesis habrá el codigo que deseemos que nos dara el numero con decimales, el orden en el que se hacen los codigos es de interior a exterior, por ejemplo "int(max(1,5.5,8,9.4))", en este ejemplo primero se cogería el numero mas grande que sería 9.4 y luego se pasaría a "int" lo cual daría 9. Los corchetes "{}" indican un espacío vacío el cual se puede rellenar mas adelante por el hecho de no saber este mismo o de que sea un valor determinado por la parsona que interactue con el programa. Primero se hace el escrito con los valores X y luego de este aún dentro de los parentesis poner ".format()" y dentro de los parentesis el str con un valor, se teienen que colocar en orden.

---
+++ numeros.py
@@ -0,0 +1,8 @@
+
+
+
+primer_numero=int(input("dime_el_primer_numero"))
+segundo_numero=int(input("dime_un_segundo_numero"))
+tercer_numero=int(input("dime_un_tercer_numero"))
+resultado=max(primer_numero,segundo_numero,tercer_numero)
+print("el_numero_mas_grande_es {}".format(resultado))

53bfd834ecdb3b703c50cfd871ac7c0e5b37729e
FoxRoig <lmnfoxhound@gmail.com>
2018-08-27 20:24:57 +0200

    Los numeros enteros son denominados "int", los numeros con decimales "float", los textos "str" o "string", si por ejemplo tenemos un "float" y queremos hacer que sea un "int" eliminandole los decimales pondremos "int()", dentro de los parentesis habrá el codigo que deseemos que nos dara el numero con decimales, el orden en el que se hacen los codigos es de interior a exterior, por ejemplo "int(max(1,5.5,8,9.4))", en este ejemplo primero se cogería el numero mas grande que sería 9.4 y luego se pasaría a "int" lo cual daría 9. Los corchetes "{}" indican un espacío vacío el cual se puede rellenar mas adelante por el hecho de no saber este mismo o de que sea un valor determinado por la parsona que interactue con el programa. Primero se hace el escrito con los valores X y luego de este aún dentro de los parentesis poner ".format()" y dentro de los parentesis el str con un valor, se teienen que colocar en orden.

---
+++ Ej. if else.py
@@ -0,0 +1,5 @@
+number_to_guess=9
+user_number= int(input("Adivina un numero:"))
+if number_to_guess==user_number:
+    print("Enorabuena! Sigues astando solo!!! ;)")
+else : print("Perdiste viejo puto!!")
